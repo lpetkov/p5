c7f4f9ee67ffce1fbf680e11ac5e6dd660c385b5
lpetkov <31132490+lpetkov@users.noreply.github.com>
2017-11-10 08:23:45 +0200

    update folder structure, assignment 3

--- kadenze/assignment3/sketch.js
+++ kadenze/assignment3/sketch.js
@@ -1,16 +1,6 @@
 
 // Assignment 3 by Lachezar Petkov
-/* 
-requirements to use:
- 2+ conditionals
- mouseX/mouseY
- respond to pmouseX, pmouseY
- respond to mousePressed()
- extreme change in mouse movement (Large or fast mouse movements should trigger a big visual response from your expressive eyes.)
- no errors
- craft in drawing the eyes
 
-*/
 
 var iris_r = 100;
 var eye_r = 250;
@@ -18,15 +8,25 @@ var eye_r = 250;
 function setup() {
   createCanvas(1024, 768);
   background(0);
-
-}
+} 
 
 function draw() {
-if (!mouseIsPressed) {
-  var pupil_r = iris_r / 2;
-} else {
-  var pupil_r = iris_r / 4; 
+
+var mouse_diff = mouseX - pmouseX;
+
+if (mouse_diff > 240) {
+  print("great");
+  eye_r += 20;
 }
+
+if (!mouseIsPressed) {
+  var pupil_r = map(mouseX, 0, 1024, 25, 5);
+} else {
+  var pupil_r = map(pmouseY, 1024, 0, 5, 15); 
+}
+var r = map(mouseX, 0, 1024, 30, 50);
+var iris_r = map(mouseX, 0, 1024, 90, 110);
+
   noStroke();
 //left eye
   fill(255, 255, 255);
@@ -47,6 +47,7 @@ if (!mouseIsPressed) {
   ellipse(270, 325, 20, 20);
   fill(255, 255, 255, 90);
   ellipse(225, 360, 40, 40);
+  triangle(290, 360, 300, 380, 340, 380);
 
 //right eye
   fill(255);
@@ -67,7 +68,8 @@ if (!mouseIsPressed) {
   fill(255, 255, 255, 125);
   ellipse(620, 325, 20, 20);
   fill(255, 255, 255, 90);
-  ellipse(575, 360, 40, 40);
+  ellipse(575, 360, r, r);
+  triangle(630, 360, 645, 380, 680, 380);
 
 //left eyelid
   fill(0);
@@ -77,8 +79,14 @@ if (!mouseIsPressed) {
   fill(0);
   rect(450, 200, 300, 100);
 
-
-
+//bottom eyelitds
+fill(0);
+beginShape();
+vertex(20, 380);
+vertex(40, 580);
+vertex(780, 580);
+vertex(780, 400);
+endShape(CLOSE);
 }
 
 function mousePressed() {
